d600f31ec08e34942c9b10d7cda61c30a95fd94d
zambbo <zambbo@LAPTOP-S2GJUHM4.localdomain>
2023-02-18 18:47:50 +0900

    boj [15655]

--- backtracking/n_m_6/target/debug/deps/n_m_6-38bf5fa46bf15193.d
+++
@@ -1,5 +0,0 @@
-/home/zambbo/rust_ps/backtracking/n_m_6/target/debug/deps/n_m_6-38bf5fa46bf15193: src/main.rs
-
-/home/zambbo/rust_ps/backtracking/n_m_6/target/debug/deps/n_m_6-38bf5fa46bf15193.d: src/main.rs
-
-src/main.rs:
--- backtracking/n_m_6/target/debug/n_m_6.d
+++
@@ -1,1 +0,0 @@
-/home/zambbo/rust_ps/backtracking/n_m_6/target/debug/n_m_6: /home/zambbo/rust_ps/backtracking/n_m_6/src/main.rs
